b29b35e7f51a1c02a848c92f828025b581aee4be
kevin <kevin.minyu@gmail.com>
2026-02-04 15:34:51 +0800

    Fix column assignment in app7_monthstart.py

--- app7_monthstart.py
+++ app7_monthstart.py
@@ -312,6 +312,7 @@ if __name__ == "__main__":
             df = split_row_to_rows(df)
            
             df.columns=[
+                'Date',
                 '1WEEK',
                 '1MONTH',
                 '2MONTH',
