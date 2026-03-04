737592ffbc32d54e709ff2bd46afa10cfc1b96f4
kevin <kevin.minyu@gmail.com>
2026-03-04 15:41:13 +0800

    Update app7_monthstart.py

--- app7_monthstart.py
+++ app7_monthstart.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
             #     '10MONTH',
             #     '11MONTH',
             #     '12MONTH']
-            df.insert(loc=0, column='Date', value=['2026-03-02','2026-03-03'])
+            # df.insert(loc=0, column='Date', value=['2026-03-02','2026-03-03'])
             print('324 df', df)
             
             # df.insert(0, 'Date', date_list)
